c2e992f940a8c6fecb7d1b0fc6bf70b71eaaf33d
Sam Title <samstitle@gmail.com>
2025-03-05 23:31:48 -0500

    Started getting atuin and other nice stuff working

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -13,11 +13,51 @@
     google-chrome
     code-cursor
     zed-editor
+    atuin
+    eza
+    bat
   ];
 
   # Common programs
   programs._1password.enable = true;
   programs._1password-gui = {
+    enable = true;
+  };
+
+ programs.git = {
+    enable = true;
+    lfs.enable = true;
+    config = {
+      push = {
+        autoSetupRemote = true;
+      };
+    };
+  };
+
+  # enable and configure zsh, set as default shell
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    shellInit = ''
+      eval "$(atuin init zsh)"
+    '';
+  };
+  users.defaultUserShell = pkgs.zsh;
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+  services.atuin = {
     enable = true;
   };
 }
