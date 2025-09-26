e4f9a05aa9174e6666122de79e0dc79016e6acc2
Sam Title <samstitle@gmail.com>
2025-09-25 22:49:00 -0400

    Trying to re-enable ccache

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -11,38 +11,38 @@
   # This setting is necessary for Nickel-lang organist
   nix.settings.allow-import-from-derivation = true;
 
-  # # Add ccache directory to sandbox paths
-  # nix.settings.extra-sandbox-paths = [ "/var/cache/ccache" ];
+  # Add ccache directory to sandbox paths
+  nix.settings.extra-sandbox-paths = [ "/var/cache/ccache" ];
 
-  # # Add ccache overlay
-  # nixpkgs.overlays = [
-  #   (self: super: {
-  #     ccacheWrapper = super.ccacheWrapper.override {
-  #       extraConfig = ''
-  #         export CCACHE_COMPRESS=1
-  #         export CCACHE_DIR="/var/cache/ccache"
-  #         export CCACHE_UMASK=007
-  #         export CCACHE_SLOPPINESS=random_seed
-  #         if [ ! -d "$CCACHE_DIR" ]; then
-  #           echo "====="
-  #           echo "Directory '$CCACHE_DIR' does not exist"
-  #           echo "Please create it with:"
-  #           echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
-  #           echo "  sudo chown root:nixbld '$CCACHE_DIR'"
-  #           echo "====="
-  #           exit 1
-  #         fi
-  #         if [ ! -w "$CCACHE_DIR" ]; then
-  #           echo "====="
-  #           echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
-  #           echo "Please verify its access permissions"
-  #           echo "====="
-  #           exit 1
-  #         fi
-  #       '';
-  #     };
-  #   })
-  # ];
+  # Add ccache overlay
+  nixpkgs.overlays = [
+    (self: super: {
+      ccacheWrapper = super.ccacheWrapper.override {
+        extraConfig = ''
+          export CCACHE_COMPRESS=1
+          export CCACHE_DIR="/var/cache/ccache"
+          export CCACHE_UMASK=007
+          export CCACHE_SLOPPINESS=random_seed
+          if [ ! -d "$CCACHE_DIR" ]; then
+            echo "====="
+            echo "Directory '$CCACHE_DIR' does not exist"
+            echo "Please create it with:"
+            echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+            echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+            echo "====="
+            exit 1
+          fi
+          if [ ! -w "$CCACHE_DIR" ]; then
+            echo "====="
+            echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+            echo "Please verify its access permissions"
+            echo "====="
+            exit 1
+          fi
+        '';
+      };
+    })
+  ];
 
   # Common packages
   environment.systemPackages = with pkgs; [
@@ -137,8 +137,8 @@
   };
 
   # Enable ccache for faster compilation
-  # programs.ccache = {
-  #   enable = true;
-  #   cacheDir = "/var/cache/ccache";
-  # };
+  programs.ccache = {
+    enable = true;
+    cacheDir = "/var/cache/ccache";
+  };
 }
